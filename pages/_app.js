2fa17f11edfd5b332a022202a0799d84320ddcd9
sdubrez <22425976+imsamdez@users.noreply.github.com>
2022-12-22 11:56:21 +0100

    setup bold example

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
           })(window);`,
         }}
       />
-      ;<Component {...pageProps} />
+      <Component {...pageProps} />
     </>
   );
 }
